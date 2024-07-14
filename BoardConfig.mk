cb8a67bd68db2bfb49b9d5bf0c46e0cf88297f3b
(Dyneteve <dyneteve@hentaios.com>
2024-07-14 09:13:20 +0000

    garnet: Enable AOSP surfaceflinger
    - AOSP SurfaceFlinger is smoother than CAF
    
    Change-Id: Id671da2363fb09303a7d7241d5ae95e55ff16475

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -203,6 +203,9 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
+# SurfaceFlinger
+TARGET_USE_AOSP_SURFACEFLINGER := true
+
 # System properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/props/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/props/product.prop
