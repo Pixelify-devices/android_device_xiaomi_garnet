a795b5afba4deab03b32727c8745480a17632b8a
AdarshGrewal <adarshgrewal@gmail.com>
2025-12-13 00:50:02 +0530

    garnet: Update overlays for ASCP
     - Move system_ext overlays to product as ASCP ships a overlay config in system_ext which causes extra overlays in system_ext fail to load
    
    Change-Id: I40e9e08f8f32f76d77e4ce1babb37010cbd0a8d8

--- device.mk
+++ device.mk
@@ -288,13 +288,12 @@ $(foreach sku, CN GL JP, \
 PRODUCT_PACKAGES += \
     ApertureOverlayGarnet \
     CarrierConfigOverlayGarnet \
+    CustomSettingsOverlayGarnet \
+    CustomSystemUIOverlayGarnet \
     FrameworkOverlayGarnet \
     FrameworkOverlayGarnetGLEsim \
     FrameworkOverlayGarnetJPEsim \
     Launcher3DeviceOverlayGarnet \
-    LineageSDKOverlayGarnet \
-    LineageSettingsOverlayGarnet \
-    LineageSystemUIOverlayGarnet \
     NfcOverlayGarnet \
     SettingsOverlayGarnet \
     SettingsProviderOverlayGarnetPoco \
